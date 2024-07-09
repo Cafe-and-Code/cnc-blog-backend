d9e459c03328b2196c30f636044e83069abb4ad5
Horace <49237174+MHieu128@users.noreply.github.com>
2024-07-09 15:27:21 +0700

    Update to connect DB

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,10 @@
+// Purpose: DataContext class to connect to the database.
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        
+    }
+}
