1ea13de0155e3a00772e3654353102b3bc269155
Horace <minhhieudn98@gmail.com>
2024-09-22 02:28:32 +0700

    hash password extension

---
+++ Blog.API/Commons/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Blog.API.Commons
+{
+    public static class StringExtension
+    {
+        public static string EncryptPassword(this string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var passwordBytes = Encoding.UTF8.GetBytes(password);
+                var hashedBytes = sha256.ComputeHash(passwordBytes);
+                var hashedPassword = Encoding.UTF8.GetString(hashedBytes);
+                return hashedPassword;
+            }
+        }
+    }
+}
