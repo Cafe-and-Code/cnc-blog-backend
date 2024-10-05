70a0126d7434f0eba390c972ca4c0607564ab474
Horace <minhhieudn98@gmail.com>
2024-10-05 23:55:36 +0700

    Init controll upload image

--- Blog.API/Migrations/20241005164650_Add image table.Designer.cs
+++ Blog.API/Migrations/20241005164650_Add image table.Designer.cs
@@ -4,6 +4,7 @@ using Blog.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Blog.API.Migrations
 {
     [DbContext(typeof(BlogDbContext))]
-    partial class BlogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241005164650_Add image table")]
+    partial class Addimagetable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
