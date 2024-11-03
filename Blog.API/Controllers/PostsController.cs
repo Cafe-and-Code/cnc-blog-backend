bd347f29db864c1a38a29f4625a5ab2529e59598
Horace <minhhieudn98@gmail.com>
2024-11-04 04:04:17 +0700

    Clean source

--- Blog.API/Controllers/PostsController.cs
+++ Blog.API/Controllers/PostsController.cs
@@ -5,7 +5,6 @@ using Blog.API.Models.DTO;
 using Blog.API.Repositories.IRepository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 
 namespace Blog.API.Controllers
 {
